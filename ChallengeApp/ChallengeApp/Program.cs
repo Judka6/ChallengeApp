5d80503e50b33b5e6f93a112e47527390c6955ee
Judka6 <judytakietlinska@gmail.com>
2024-01-17 14:07:12 +0100

    Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Kobieta poniżej 30 lat
+// Ewa, lat 30
+// Niepełnoletni mężczyzna
+
+string name = "Ewa";
+int age = 28;
+bool isWoman = true;
+
+if (isWoman == true && age < 30) 
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 36)
+{
+    Console.WriteLine("Ewa, lat 36");     
+}
+else if (isWoman == false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Brak danych");
+}
